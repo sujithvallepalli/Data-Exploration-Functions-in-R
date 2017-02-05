c39aaa976fa424dedb97a3ee213cdd8976e259e6
Sujith Vallepalli <sujithkumarvallepalli@gmail.com>
2017-02-05 18:00:46 -0500

    Update Data Exploration Functions.r

--- Data Exploration Functions.r
+++ Data Exploration Functions.r
@@ -19,3 +19,67 @@ return(df1)
 #Loading library of MASS to check data in that package
 library(MASS)
 missing_val(survey)
+
+#########################################################################################
+
+##Below are few visually explorable functions beyond simple plot() 
+
+#Install and Load MASS package and view the data set to get details of the data
+install.packages("MASS")
+library("MASS")
+View(survey)
+#help(survey)
+
+#Install and load packages plotly, ggplot2 and corrgram. These are the packages to plot and visualize the data
+install.packages("plotly")
+install.packages("ggplot2")
+install.packages("corrgram")
+library(corrgram)
+library(plotly)
+library(ggplot2)
+
+#Attaching this data set as we use only this data set here
+attach(survey)
+
+#Plot 1
+plot_ly(survey,x=W.Hnd,y=Wr.Hnd,type="box")
+## This interactive box plot shows that Left handed students are having more Span on their Writing hand compared to Right handed Students.
+#Average writing hand span is almost same as the median students
+#Most of the Left handed students are having span of their writing hand more than average left handed students
+
+
+#Plot 2
+ggplot(data=survey, aes(x=Age,y=Pulse,color=Sex))+ facet_grid(. ~ Smoke)+ geom_point(aes(shape=factor(W.Hnd)))
+#This graph gives the good information of the data set as it can use most of the variables
+#INsight1: Majority of the students have age less than 20
+#Insight2: Very few students are left handed
+
+
+#Plot 3
+#Removing NA from Smoke
+a=!is.na(survey$Smoke)
+b=survey[a,]
+qplot(Pulse,Smoke,data=b,geom="jitter",facets=.~Exer)
+#Heavy Smokers do exercise
+#Most of the students do not smoke
+#There are very few students who either smoke occasionally or Regularly AND DO NOT EXERCISE. These students must be motivated to either doing exercise or quit smoking
+
+
+#Plot 4
+symbols(Wr.Hnd,NW.Hnd,circles =Age ,inches = 0.1,bg="red")
+#There are Two students whose age is far away from others
+#There are Three students whose Writing hand span is different from their Non Writing hand (Wr.Hnd,NW.Hnd)=(15,13),(14,16),(18,13)
+
+
+#Plot 5
+#Let us observe  Right Handed people and Left handed people Clapping Styles
+ggplot(data=survey, aes(x=Fold,y=Height,color=Clap))+ facet_grid(. ~ W.Hnd)+geom_point(aes(shape=factor(Sex)))
+#Most of the Right Handed People have their RIght Hand on top when Clapping
+#Same trend is not present among Left handed people(This may be a bias as number of Left handed are less compared to right)
+
+
+#Plot 6
+corrgram(survey,order=T,upper.panel = panel.pie)
+#This Correlogram tells that there is High Correlation between Writing Hand and Non Writing Hand
+#Also There is a correlation between span of hand and Height of the student
+
